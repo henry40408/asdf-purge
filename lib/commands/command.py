e328fdcfaaa790ce50af369f1825e5b441c1b88b
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2022-02-19 17:37:55 +0800

    fix: use asdf uninstall and plugin remove

--- lib/commands/command.py
+++ lib/commands/command.py
@@ -2,7 +2,6 @@
 
 import os
 import re
-import shutil
 
 from optparse import OptionParser
 from pathlib import Path
@@ -25,17 +24,18 @@ def main():
     reserved_plugins = set(t[0] for t in tuples)
     for plugin_dir in Path(os.path.expanduser('~/.asdf/installs')).iterdir():
         plugin = os.path.basename(plugin_dir)
+        for version_dir in Path(plugin_dir).iterdir():
+            version = os.path.basename(version_dir)
+            if (plugin, version) not in tuples:
+                c = f'asdf uninstall {plugin} {version}'
+                print(c)
+                if options.force:
+                    os.system(c)
         if plugin not in reserved_plugins:
-            print('- {}'.format(plugin_dir))
+            c = f'asdf plugin remove {plugin}'
+            print(c)
             if options.force:
-                shutil.rmtree(plugin_dir)
-        else:
-            for version_dir in Path(plugin_dir).iterdir():
-                version = os.path.basename(version_dir)
-                if (plugin, version) not in tuples:
-                    print('- {}'.format(version_dir))
-                    if options.force:
-                        shutil.rmtree(version_dir)
+                os.system(c)
 
 
 if __name__ == '__main__':
